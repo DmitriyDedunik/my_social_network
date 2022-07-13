30c31b9e12524ea01e7f89beaf23374bf19b4d8e
Dmitry <dedunik.dmitry@yanex.ru>
2022-07-13 17:09:00 +0300

    сохранение about

--- msn_dj/accounts/forms.py
+++ msn_dj/accounts/forms.py
@@ -1,5 +1,5 @@
 from .models import About
-from django.forms import ModelForm, SelectDateWidget
+from django.forms import HiddenInput, ModelForm, SelectDateWidget, DateInput
 from datetime import datetime
 
 class AboutForm(ModelForm):
@@ -10,7 +10,9 @@ class AboutForm(ModelForm):
         year_range = tuple([i for i in range(cur_year - 100, cur_year + 1)])
 
         model = About
-        fields = ('date_birth', 'city', 'hobbies', 'main_photo')
+        fields = ('date_birth', 'city', 'hobbies', 'main_photo', 'user')
         widgets = {
-            'date_birth': SelectDateWidget(years=year_range)
+            'date_birth': SelectDateWidget(years=year_range),
+            'user': HiddenInput(),
+            # 'date_birth': DateInput(attrs={'type':'date'})
             }
