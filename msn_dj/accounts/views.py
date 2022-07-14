dc6ef805ce48a93cb39fcdbfc4c015d5f584e5c6
Dmitry <dedunik.dmitry@yanex.ru>
2022-07-14 21:19:24 +0300

    пробую добавить апгрейд

--- msn_dj/accounts/views.py
+++ msn_dj/accounts/views.py
@@ -1,5 +1,6 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ObjectDoesNotExist
@@ -8,10 +9,26 @@ from .models import About
 from .forms import AboutForm
 from django.views.generic.edit import CreateView, UpdateView
 
+
 class SignupView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+class AboutUpdateView(UpdateView):
+    template_name = 'about.html'
+    form_class = AboutForm
+    model = About
+    
+    def get_success_url(self):
+        return reverse('accounts:profile')
+
+
+class AboutCreateView(CreateView):
+    template_name = 'about.html'
+    form_class = AboutForm
+    success_url = reverse_lazy('accounts:profile')
 
 
 def profile(request):
@@ -21,10 +38,24 @@ def profile(request):
 
 def profile_update(request):
     template_name = 'about.html'
+    # try:
+    #     about_db = About.objects.get(user_id=request.user.id)
+    #     context = {'form': AboutUpdateView.as_view()}
+    #     return render(request, template_name, context)
+    # except ObjectDoesNotExist:
+    #     context = {'form': AboutCreateView.as_view()}
+    #     return render(request, template_name, context)
+
     if request.method == "POST":
-        # about = About.objects.get(user_id=request.user.id)
+
         about = request.POST.dict()
         about['user'] = request.user
+        try:
+            about_db = About.objects.get(user_id=request.user.id)
+            about['id'] = about_db.id
+            force_update=True
+        except ObjectDoesNotExist:
+            force_insert=True
         form = AboutForm(about, request.FILES)
         if form.is_valid():
             form.save()
